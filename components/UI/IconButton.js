ab7211a8ba81e773c4e39cf9ecbb16114a92072b
amr4356 <amrooo73321@gmail.com>
2024-10-15 17:16:34 +0300

    193. Adding a Header Button

---
+++ components/UI/IconButton.js
@@ -0,0 +1,24 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+export default function IconButton({ icon, size, color, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Ionicons name={icon} color={color} size={size} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddin: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
